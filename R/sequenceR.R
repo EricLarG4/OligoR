108b109abffdd6c111313aa75e549f902273564b
EricLarG4 <e.largy@protonmail.com>
2021-06-18 18:32:45 +0200

    Update sequenceR.R
    All calculation in a unique list (which is the output)

--- R/sequenceR.R
+++ R/sequenceR.R
@@ -16,65 +16,49 @@
 
 sequenceR <- function(z, K, sequence, nX.user.input, nX.select, mol = 1){
 
-  #charge----
-  z <- as.numeric(z)
-
-  #number potassium adducts----
-  K <- as.numeric(K)
-
   #strand molecularity----
   mol <- as.numeric(mol)
 
   #nucleotide extraction from sequence----
   seq2 <- data.frame(number=1:1, string=c(sequence), stringsAsFactors = F)
 
-  nbA <- str_count(seq2$string, "A")
-
-  nbT <- str_count(seq2$string, "T")
-
-  nbG <- str_count(seq2$string, "G")
-
-  nbC <- str_count(seq2$string, "C")
+  nts <- list(
+    z = as.numeric(z),
+    K = as.numeric(K),
+    nbA = str_count(seq2$string, "A"),
+    nbT = str_count(seq2$string, "T"),
+    nbG = str_count(seq2$string, "G"),
+    nbC = str_count(seq2$string, "C")
+  )
 
   #Number of nucleotides per strand----
-  nb_nt <- nbA + nbT + nbG + nbC
+  nts$nb_nt <- nts$nbA + nts$nbT + nts$nbG + nts$nbC
 
   #Number of phosphates per strand----
-  nb_PO <- nb_nt - 1
+  nts$nb_PO <- nts$nb_nt - 1
 
-  #Total number of nucleotides----
-  nb_nt <- nb_nt*mol
+  #Total number of nucleotides and phosphates----
+  nts <- lapply(nts, function(x){x*mol})
 
-  #Total number of phosphates----
-  nb_PO <- nb_PO*mol
-
-  #total number of nucleotides----
-  nbA <- nbA * mol
-
-  nbT <- nbT * mol
-
-  nbG <- nbG * mol
-
-  nbC <- nbC * mol
 
   #Neutralized phosphate
   #(takes into account charge and potassium adducts that bring positive charges)
-  nb_POH <- nb_PO - z - K
+  nts$nb_POH <- nts$nb_PO - nts$z - nts$K
 
   #number of atoms----
-  nC <- nbA*10 + nbG*10 + nbC*9 + nbT*10
+  nts$nC <- nts$nbA*10 + nts$nbG*10 + nts$nbC*9 + nts$nbT*10
 
   #This is the total amount of hydrogen across isotopes (among which nX are exchangeable)
   #Charge taken into account here, so H will not be taken out when calculating m/z
-  nH <- nbA*12 + nbG*12 + nbC*12 + nbT*13 + 1*mol - z - K
+  nts$nH <- nts$nbA*12 + nts$nbG*12 + nts$nbC*12 +nts$nbT*13 + 1*mol - nts$z - nts$K
 
-  nO <- nbA*5 + nbG*6 + nbC*6 + nbT*7 - 2*mol
+  nts$nO <- nts$nbA*5 + nts$nbG*6 + nts$nbC*6 + nts$nbT*7 - 2*mol
 
-  nN <- nbA*5 + nbG*5 + nbC*3 + nbT*2
+  nts$nN <- nts$nbA*5 + nts$nbG*5 + nts$nbC*3 + nts$nbT*2
 
-  nP <- nb_PO
+  nts$nP <- nts$nb_PO
 
-  nK <- K
+  nts$nK <- nts$K
 
   #number of exchanging sites----
 
@@ -84,27 +68,18 @@ sequenceR <- function(z, K, sequence, nX.user.input, nX.select, mol = 1){
   #presets
   if(nX.user.input == ''){
     if(nX.select == 'A'){
-      nX <- nb_PO + 2 + nbA*2 + nbT*1 + nbG*3 + nbC*2 - z
+      nts$nX <- nts$nb_PO + 2 + nts$nbA*2 + nts$nbT*1 + nts$nbG*3 + nts$nbC*2 - nts$z
     } else {
       if(nX.select == 'B'){
-        nX <- 2 + nbA*2 + nbT*1 + nbG*3 + nbC*2
+        nts$nX <- 2 + nts$nbA*2 + nts$nbT*1 + nts$nbG*3 + nts$nbC*2
       } else {
-        nX <- nbA*2 + nbT*1 + nbG*3 + nbC*2
+        nts$nX <- nts$nbA*2 + nts$nbT*1 + nts$nbG*3 + nts$nbC*2
       }
     }
   } else {
-    nX <- nX.user
+    nts$nX <- nX.user
   }
 
-  #output----
-  output <- list(z=z, K=K,
-                 nbA=nbA, nbT=nbT, nbG=nbG, nbC=nbC,
-                 nb_nt=nb_nt,
-                 nb_PO=nb_PO, nb_POH=nb_POH,
-                 nC=nC, nH=nH, nO=nO, nN=nN, nP=nP, nK=nK,
-                 nX = nX
-  )
-
-  return(output)
+  return(nts)
 
 }
