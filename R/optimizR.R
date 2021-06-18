0e3570b0222c7c158f850f10474f684fbe237b69
EricLarG4 <e.largy@protonmail.com>
2021-06-18 18:38:15 +0200

    Update optimizR.R
    Minor changes
    - use of dtplyr
    - change sequencer to seq to follow changes in other functions

--- R/optimizR.R
+++ R/optimizR.R
@@ -1,62 +1,66 @@
 
 
-
-#to be removed once packaged:
-# library(tidyverse)
-# source("R/peakpickR.R")
-# source("R/sequenceR.R")
-# source("R/peakpositionR.R")
-
 #optimization (minimization) of peakpositionR vs. the peack-picked experimental distribution
 
 #Optimizer----
-optimizer <- function(par, z, raw.data, nX.select, K,
+optimizer <- function(par, z, nX.select, K,
                       nrPeaks.user, bi,
-                      pp, sequencer, massr){
+                      pp, seq, massr){
 
   library(DescTools)
-  library(tidyverse)
+  library(dtplyr)
 
   #theoretical distribution (no optim)
+
   if (isTRUE(bi)) {
+
     #bimodal distribution
+
     #first distribution
     theo.1 <- peak.positionR(nrPeaks.user=nrPeaks.user,
                              DC=par[1], #deuteration of the distribution (optimizable)
-                             seq=sequencer,
+                             seq=seq,
                              MonoMW=massr$MonoMW) %>%
-      mutate(Iso.Pattern = Iso.Pattern * par[3]) #scaling to abundance of the distribution (optimizable)
+      lazy_dt(.) %>%
+      mutate(Iso.Pattern = Iso.Pattern * par[3]) %>%  #scaling to abundance of the distribution (optimizable)
+      as.data.frame()
 
+    #second distribution
     theo <- peak.positionR(nrPeaks.user=nrPeaks.user, #second distribution
                            DC=par[2], #deuteration of the distribution (optimizable)
-                           seq=sequencer,
+                           seq=seq,
                            MonoMW=massr$MonoMW) %>%
       mutate(Iso.Pattern = Iso.Pattern * par[4]) %>% #scaling to abundance of the distribution (optimizable)
       rbind(theo.1) %>% #binding to first distribution
+      lazy_dt(.) %>%
       group_by(mz.th) %>% #grouping by mass
-      mutate(Iso.Pattern = sum(Iso.Pattern)) #summing intensities of each mass group to output a single series
+      mutate(Iso.Pattern = sum(Iso.Pattern)) %>%  #summing intensities of each mass group to output a single series
+      as.data.frame()
   } else {
+
     #monomodal distribution
     theo <- peak.positionR(nrPeaks.user = nrPeaks.user,
                            DC=par[1], #deuteration of the distribution (optimizable)
-                           seq = sequencer,
+                           seq = seq,
                            MonoMW = massr$MonoMW)
   }
 
+
   #merging of optimized theoretical data to experimental data
   #to calculate sum of squares of differences
-  merged <- pp %>%
+  merged <- lazy_dt(pp) %>%
     #mz rouding to bin theoretical and experimental data together
     mutate(rmz = RoundTo(mz, multiple = 1/z, FUN = round)) %>%
     filter(peak > 0) %>% #removes 0 intensity data
     left_join(theo %>% #joining by mass bins (bins width based on charge z)
-                mutate(rmz = RoundTo(mz.th, multiple = 1/sequencer$z, FUN = round)),
+                mutate(rmz = RoundTo(mz.th, multiple = 1/seq$z, FUN = round)),
               by = 'rmz') %>%
     #grouping by timepoint
-    group_by(colorscale, Species, filename, min.time, max.time, min.scan, max.scan, min.mz, max.mz, rmz) %>%
-    mutate(diff.2 = (Iso.Pattern - intensum)^2) %>% #square of differences
+    # group_by(colorscale, Species, filename, min.time, max.time, min.scan, max.scan, min.mz, max.mz, rmz) %>%
+    mutate(diff.2 = (Iso.Pattern - intensum)^2) %>% #square of differences of each point
     ungroup() %>%
-    summarise(sum.diff.2 = sum(diff.2)) #sum of squares of differences
+    summarise(sum.diff.2 = sum(diff.2)) %>%  #sum of squares of differences
+    as.data.frame()
 
   output <- as.vector(merged$sum.diff.2)
 
@@ -68,5 +72,8 @@ optimizer <- function(par, z, raw.data, nX.select, K,
   }
 
 }
+
+
+
 
 
